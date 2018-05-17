178a856a645eed3bc1252018906f7f859bd6e33f
luongnhuyen <luongnhuyen@gmail.com>
2018-05-17 23:45:02 +0700

    initial

--- Web03/Homework/app.py
+++ Web03/Homework/app.py
@@ -11,24 +11,10 @@ mlab.connect()
 def index():
     return render_template('index.html')
 
-@app.route('/search/<g>')
-def search(g):
-    all_service = Service.objects(gender=g, yob__lte=2000, address__icontains="Hà Nội")
-    return render_template('search.html', all_service=all_service)
-
 @app.route('/admin')
 def admin():
     all_service = Service.objects()
     return render_template('admin.html',all_service = all_service)
-
-@app.route('/delete/<service_id>')
-def delete(service_id):
-    service_to_delete = Service.objects.with_id(service_id)
-    if service_to_delete is not None:
-        service_to_delete.delete()
-        return redirect(url_for('admin'))
-    else:
-        return "Service not found"
 
 @app.route('/new-service',methods=['GET','POST'])
 def creat():
